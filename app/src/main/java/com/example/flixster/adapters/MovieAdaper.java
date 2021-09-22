8237aa560ffa7117155d7b0ae9d5d1a670c69de1
ahirpara2000 <ahirpara2000@gmail.com>
2021-09-22 19:19:46 -0400

    Added transition names

--- app/src/main/java/com/example/flixster/adapters/MovieAdaper.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdaper.java
@@ -15,6 +15,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.flixster.R;
 import com.example.flixster.models.DetailPage;
 import com.example.flixster.models.Movie;
@@ -95,6 +97,7 @@ public class MovieAdaper extends  RecyclerView.Adapter<MovieAdaper.ViewHolder> {
 
             Glide.with(context)
                     .load(imageUrl)
+                    .apply(RequestOptions.bitmapTransform(new RoundedCorners(14)))
                     .placeholder(R.drawable.placeholder)
                     .error(R.drawable.placeholder)
                     .into(ivPoster);
--- app/src/main/java/com/example/flixster/models/DetailPage.java
+++ app/src/main/java/com/example/flixster/models/DetailPage.java
@@ -12,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.R;
@@ -94,6 +96,7 @@ public class DetailPage extends AppCompatActivity {
                 .into(backDrop);
         Glide.with(this)
                 .load(movie.getPosterPath())
+                .apply(RequestOptions.bitmapTransform(new RoundedCorners(14)))
                 .placeholder(R.drawable.placeholder)
                 .error(R.drawable.placeholder)
                 .into(poster);
